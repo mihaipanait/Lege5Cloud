3884d9a3b84c66b2c01c8a87a5aa77c12f5149a4
Mihai Panait <panait_mihai@yahoo.com>
2014-08-25 17:24:16 +0300

    added angular, wow and animate css

--- js/main.js
+++ js/main.js
@@ -19,6 +19,20 @@ $(document).ready(function($) {
       smoothScroll.init();
 });
 
+// wow-js
+$(document).ready(function($) {
+  var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true        // act on asynchronously loaded content (default is true)
+  });
+  wow.init();
+  wow.sync();
+});
+
 // Owl Top-slider
 $(document).ready(function($) {
       $("#owl-top-slider").owlCarousel({
