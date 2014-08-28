4766184dbac1a859f3463394fea3dae6b326dda3
Mihai Panait <panait_mihai@yahoo.com>
2014-08-28 18:35:13 +0300

    created a view for each state

--- js/app.js
+++ js/app.js
@@ -1,25 +1,19 @@
-var app = angular.module("app",['ui.router']);
+var app = angular.module("app",['ui.router', 'ct.ui.router.extras']);
 
-app.config(function($stateProvider, $urlRouterProvider){ 
+app.config(function($stateProvider, $urlRouterProvider, $stickyStateProvider){ 
+	$stickyStateProvider.enableDebug(true);
 
-	$urlRouterProvider.otherwise("index")
+	$urlRouterProvider.otherwise("/")
 
 	$stateProvider
 	.state('root', {
-		url: '',
-		abstract: true,
-		views: {
-			"features-view": { templateUrl: "features-panels" },
-			"pricing-view": { templateUrl: "pricing-panels" }
-		}
-	})
-
-	.state('root.index', {
-		url: '/',
+		url: "/",
 		views: {
 			"features-view@": { templateUrl: "features-panels" },
 			"pricing-view@": { templateUrl: "pricing-panels" }
-		}
+		},
+		//deepStateRedirect: true,
+    	//sticky: true
 	})
 
 	.state('root.features', {
@@ -32,51 +26,56 @@ app.config(function($stateProvider, $urlRouterProvider){
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#features', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.feature-details-01', {
 		url: "/feature-01",
 		views: {
-			"features-view@": { templateUrl: "feature-details-01" }
+			"features-details-view@": { templateUrl: "feature-details-01" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#features', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.feature-details-02', {
 		url: "/feature-02",
 		views: {
-			"features-view@": { templateUrl: "feature-details-02" }
+			"features-details-view@": { templateUrl: "feature-details-02" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#features', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.feature-details-03', {
 		url: "/feature-03",
 		views: {
-			"features-view@": { templateUrl: "feature-details-03" }
+			"features-details-view@": { templateUrl: "feature-details-03" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#features', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.feature-details-04', {
 		url: "/feature-04",
 		views: {
-			"features-view@": { templateUrl: "feature-details-04" }
+			"features-details-view@": { templateUrl: "feature-details-04" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#features', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing', {
@@ -87,61 +86,67 @@ app.config(function($stateProvider, $urlRouterProvider){
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing-details-01', {
 		url: "/pricing-01",
 		views: {
-			"pricing-view@": { templateUrl: "pricing-details-01" }
+			"pricing-details-view@": { templateUrl: "pricing-details-01" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing-details-02', {
 		url: "/pricing-02",
 		views: {
-			"pricing-view@": { templateUrl: "pricing-details-02" }
+			"pricing-details-view@": { templateUrl: "pricing-details-02" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing-details-03', {
 		url: "/pricing-03",
 		views: {
-			"pricing-view@": { templateUrl: "pricing-details-03" }
+			"pricing-details-view@": { templateUrl: "pricing-details-03" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing-details-04', {
 		url: "/pricing-04",
 		views: {
-			"pricing-view@": { templateUrl: "pricing-details-04" }
+			"pricing-details-view@": { templateUrl: "pricing-details-04" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 
 	.state('root.pricing-details-05', {
 		url: "/pricing-05",
 		views: {
-			"pricing-view@": { templateUrl: "pricing-details-05" }
+			"pricing-details-view@": { templateUrl: "pricing-details-05" }
 		},
 		onEnter: function(){
 			var options = { speed: 100, easing: 'easeOutCubic',updateURL: false, offset: 0};
 			smoothScroll.animateScroll( null, '#pricing', options );
-		}
+		},
+		sticky: true
 	})
 });
